8b754a4501b67b709704b884df831ba232a4e487
Andrii <Popov.Andrey80@gmail.com>
2023-10-14 23:59:23 +0300

    add 9th task

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,24 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  button: document.querySelector(".change-color"),
+  color: document.querySelector(".color"),
+  body: document.querySelector("body")
+}
+
+refs.button.addEventListener("click", onChangeColor)
+
+function onChangeColor() {
+
+  const { body, color, button } = refs
+
+  const randonColor = getRandomHexColor()
+
+  color.textContent = randonColor;
+
+  body.style.backgroundColor = randonColor;
+  button.style.backgroundColor = "transparent";
+
+}
