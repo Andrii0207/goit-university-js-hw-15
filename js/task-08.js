91fb167a6194a764cb8ae04a0f51d6b16779f0a1
Andrii <Popov.Andrey80@gmail.com>
2023-10-14 23:28:49 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
 
+const form = document.querySelector(".login-form")
+
+form.addEventListener("submit", submitForm)
+
+function submitForm(e) {
+    e.preventDefault()
+
+    const { email, password } = e.currentTarget.elements;
+
+    if ((email.value && password.value) === "") {
+        return alert("Please, fill off all form fields")
+    }
+
+    const formData = new FormData(e.currentTarget)
+    const formValues = {}
+
+    formData.forEach((value, key) => formValues[key] = value)
+
+    console.log(formValues)
+    e.currentTarget.reset()
+}
